6c25de4c1b76588e4db7061bd5adaabe847b01f4
Gustavo Costa <gustavobhweb@gmail.com>
2014-06-22 18:04:07 -0300

    assa

--- application/views/adm/inicial.php
+++ application/views/adm/inicial.php
@@ -1,17 +1,28 @@
-<table class='tbl-aprovacoes'>
-	<thead>
+<table class='tbl-aprovacoes' style="margin:30px 0 0 30px">
+	<!-- <thead>
 		<tr>
 			<th>Foto</th>
 			<th colspan='2'>Ações</th>
 		</tr>
-	</thead>
+	</thead> -->
 	<tbody>
 		<?php foreach($fotos as $foto){?>
 		<tr>
-			<td><img src='<?=base_url("static/imagens/".$foto->foto)?>' width='76' height='100' /></td>
+			<td><img onclick='openImage($(this))' src='<?=base_url("static/imagens/".$foto->foto)?>' width='76' height='100' /></td>
 			<td><button style='margin:0px 0 0 6px' class='btn-cancel-iza bt-n' data-id='<?=$foto->cod_solicitacao?>' data-pessoa='<?=$foto->tbl_usuarios_cod_usuario?>'>REPROVAR</button></td>
 			<td><button style='margin:0px 0 0 6px' class='btn-conf-iza bt-y' data-id='<?=$foto->cod_solicitacao?>' data-pessoa='<?=$foto->tbl_usuarios_cod_usuario?>'>APROVAR</button></td>
 		</tr>
 		<?php }?>
 	</tbody>
 </table>
+<div id='imageShow' onclick='$(this).fadeOut();' style="display:none;position:fixed;z-index:1000;background:rgba(51,51,51,0.6);width:100%;height:100%;left:0;top:0">
+	<img width="280" height="340" id='imageShowImg' style="margin:150px 0 0 540px" />
+</div>
+<script>
+function openImage(obj)
+{
+	var src = obj.attr('src');
+	$('#imageShow').find('img').attr('src', src);
+	$('#imageShow').fadeIn();
+}
+</script>
