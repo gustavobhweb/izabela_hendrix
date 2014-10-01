dee591de30206849896124d65a30bfe7d96be445
Gustavo Carmo <gustavobhweb@gmail.com>
2014-10-01 17:05:11 -0300

    asas

--- static/js/home/enviar_foto.js
+++ static/js/home/enviar_foto.js
@@ -2,6 +2,7 @@ var i =0;
 var reader = new window.FileReader();
 function refreshImg(_this)
 {
+    $('.box-photo').animate({margin: '2% auto'});
     $('.btn-make-crop').removeAttr('disabled');
     $('.btn-make-crop').html('Salvar');
     $('.return-modal-menu').show();
@@ -50,7 +51,11 @@ function refreshImg(_this)
                         },
                         error: function()
                         {
-                            alert('Problemas na conexão!');
+                            new wmDialog('Problemas de conexão! Atualize a página e tente novamente.', {
+                                height:230,
+                                width: 330,
+                                btnCancelEnabled: false
+                            }).open();
                         }
                     });
                     imageFile = null;
@@ -67,7 +72,13 @@ function refreshImg(_this)
     }
 }
 
+function get_captcha(obj)
+{
+    $('#captcha').attr('src', '/home/getCaptcha/?' + new Date().getTime());
+}
+
 $(function(){
+
     $('.box-photo').draggable({
         handle: '.header-modal-photo'
     });
@@ -94,6 +105,7 @@ $(function(){
     });
 
     $(document).on('click', '.return-modal-menu', function(){
+        $('.box-photo').animate({margin: '8% auto'});
         $('#webcam, #crop, #flashcam').fadeOut(0, function(){
             $('#enviar-foto').fadeIn();
         });
@@ -103,7 +115,6 @@ $(function(){
         e.preventDefault();
 
         var $form = $('#form-cadastrar-solicitacao');
-
         if (!$('#ckb').is(':checked')) {
             new wmDialog('Você deve concordar deve os termos', {
                 height:230,
@@ -126,23 +137,75 @@ $(function(){
 
         var html = '<small>Insira seu <b>e-mail válido</b> para realizar o acompanhamento da entrega da sua\
                     solicitação de carteira de identificação acadêmica.</small><br><br>'
-            html += '<input type="email" id="confirm-send-with-mail" placeholder="seulogin@site.com" class="wm-input input-large" >';
-
+            html += '<input type="email" id="confirm-send-with-mail" placeholder="Digite seu e-mail" class="wm-input input-large" >';
+            html += '<input style="margin-top:8px" type="email" id="reconfirm-send-with-mail" placeholder="Confirme o e-mail" class="wm-input input-large" >';
+            html += '<div id="captcha-wrap">';
+                html += '<div class="captcha-box">';
+                    html += '<img src="/home/getCaptcha" alt="" id="captcha" style="width:100px; height: 30px; margin-top:3px;" />';
+                html += '</div>';
+                html += '<div class="text-box">';
+                    html += '<label><strong style="float:left; margin-left:10px; font-size:11px;">Digite o código acima:</strong></label>';
+                    html += '<input name="captcha-code" type="text" id="captcha-code" style="height:27px !important;" autocomplete="off">';
+                html += '</div>';
+                html += '<div class="captcha-action" style="width:10px !important; float:left;">';
+                    html += '<img src="/static/recaptcha/refresh.jpg"  alt="" id="captcha-refresh" />';
+                html += '</div>';
+            html += '</div>';
 
         var dialog = new wmDialog(html, {
             isHTML: true,
             width: 350,
-            height: 240,
+            height: 390,
             btnCancelEnabled: false,
             title: 'Insira o seu e-mail',
             onConfirm: function(btn) {
                 var $input = $('#confirm-send-with-mail');
+                var $confirm = $('#reconfirm-send-with-mail');
                 var $email = $("#hidden-email");
                 var value = $input.val();
+                var value1 = $confirm.val();
 
-                if (value && $input.is(':valid')) {
-                    $email.val(value);
-                    $form.submit();
+                if (value == '' || value == null) {
+                    new wmDialog('Complete o campo e-mail para continuar!', {
+                        height:230,
+                        width: 330,
+                        btnCancelEnabled: false
+                    }).open();
+                } else if (value && $input.is(':valid') && value == value1) {
+                    $.ajax({
+                        url: '/home/verifyCaptcha',
+                        type: 'POST',
+                        dataType: 'json',
+                        data: { 
+                            text: $('#captcha-code').val()
+                        },
+                        success: function(response){
+                            if (response.status) {
+                                $email.val(value);
+                                $form.submit();
+                            } else {
+                                new wmDialog('O texto não confere.', {
+                                    height:230,
+                                    width: 330,
+                                    btnCancelEnabled: false
+                                }).open();
+                                get_captcha();
+                            }
+                        },
+                        error: function(){
+                            new wmDialog('Problemas de conexão! Atualize a página e tente novamente.', {
+                                height:230,
+                                width: 330,
+                                btnCancelEnabled: false
+                            }).open();
+                        }
+                    });
+                } else if($input != $confirm) {
+                    new wmDialog('O e-mail não confere!', {
+                        height:230,
+                        width: 330,
+                        btnCancelEnabled: false
+                    }).open();
                 } else {
                     $input.focus();
                 }
@@ -151,5 +214,9 @@ $(function(){
 
 
         dialog.open();
-    })
+        get_captcha();
+        $('#captcha-refresh').on('click', function(){
+            get_captcha();
+        });
+    });
 });
