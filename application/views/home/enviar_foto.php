56c22d67d18035c8d9e1f09b0e366d5799d5f1b8
Gustavo Carmo <gustavobhweb@gmail.com>
2014-10-02 16:32:06 -0300

    asas

--- application/views/home/enviar_foto.php
+++ application/views/home/enviar_foto.php
@@ -76,11 +76,11 @@
                 <img src='<?=base_url("static/img/especifica.png")?>' />
                 <form id="form-cadastrar-solicitacao" method='post' enctype='multipart/form-data' style='width:780px;margin:5px 0 0 0;float:left'>
                     <input type='file' onchange='refreshImg($(this))' name='userfile' id='userfile' style='display:none' />
-                    <div style='float:left;width:300px'>
+                    <!-- <div style='float:left;width:300px'>
                         <input type='checkbox' name='ckb' id='ckb' />
                         <label for='ckb' style='font:13px Arial'>Eu me <b>responsabilizo</b> pela
                         <b>veracidade</b> desta foto e concordo com o <b><u>termo de uso</u></b> do sistema</label>
-                    </div>
+                    </div> -->
                     <input type='hidden' name='webcam-upload' value='<?=$user->matricula.".png"?>' />
                     <button id="submit-solicitacao" type='submit' name='btn-submit' class='btn-conf-iza'>ENVIAR SOLICITAÇÃO DA CARTEIRA</button>
                     <button type='button' onclick='document.location.href="<?=base_url('home/inicial')?>"' class='btn-cancel-iza'>CANCELAR</button>
